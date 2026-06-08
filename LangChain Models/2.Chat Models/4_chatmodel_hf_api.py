a190ed018817b8f841540b13d132e4d4adc88507
Yash Sherekar <yashsherekar17@gmail.com>
2026-06-09 00:49:45 +0530

    Open Source ChatModel from Hugging Face using Inference API

--- LangChain Models/2.Chat Models/4_chatmodel_hf_api.py
+++ LangChain Models/2.Chat Models/4_chatmodel_hf_api.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm = HuggingFaceEndpoint(
-    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-    task="text-generation"
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0", # HuggingFace model repository ID (like github repo, but for HuggingFace models)
+    task="text-generation" # which task to perform, it can be text-generation, text2text-generation, etc. depending on the model you are using
 )
 
 model = ChatHuggingFace(llm=llm)  # passing the llm instance to the ChatHuggingFace class
