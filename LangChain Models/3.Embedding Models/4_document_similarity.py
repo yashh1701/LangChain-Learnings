cb107edc473677cd005b28ff5eb85af7b110a0b9
Yash Sherekar <yashsherekar17@gmail.com>
2026-06-11 00:16:25 +0530

    Practical Implementation of  Document Similarity Search using Open AI

--- LangChain Models/3.Embedding Models/4_document_similarity.py
+++ LangChain Models/3.Embedding Models/4_document_similarity.py
@@ -20,9 +20,11 @@ query = 'tell me about bumrah'
 doc_embeddings = embedding.embed_documents(documents)
 query_embedding = embedding.embed_query(query)
 
+#adding index to the document embeddings for later use and then calculating the cosine similarity between the query embedding and document embeddings to get the similarity score for each document with the query
 scores = cosine_similarity([query_embedding], doc_embeddings)[0]
 
-index, score = sorted(list(enumerate(scores)),key=lambda x:x[1])[-1]
+# sort  on the basis of similarity score and get the index of the most similar document and its score with the query
+index, score = sorted(list(enumerate(scores)),key=lambda x:x[1])[-1] 
 
 print(query)
 print(documents[index])
