7d0bcc46f218725833c2bf81f96e047cb096587a
Yash Sherekar <yashsherekar17@gmail.com>
2026-06-16 23:52:52 +0530

    Practical Implementation o Messages in lanchain Prompts

--- Prompts/chatbot.py
+++ Prompts/chatbot.py
@@ -7,16 +7,16 @@ load_dotenv()
 model = ChatOpenAI()
 
 chat_history = [
-    SystemMessage(content='You are a helpful AI assistant')
+    SystemMessage(content='You are a helpful AI assistant') # system message to set the context for the conversation
 ]
 
 while True:
     user_input = input('You: ')
-    chat_history.append(HumanMessage(content=user_input))
+    chat_history.append(HumanMessage(content=user_input))  # convert user input to HumanMessage and append to chat_history
     if user_input == 'exit':
         break
     result = model.invoke(chat_history)
-    chat_history.append(AIMessage(content=result.content))
+    chat_history.append(AIMessage(content=result.content)) # convert model output to AIMessage and append to chat_history
     print("AI: ",result.content)
 
 print(chat_history)
