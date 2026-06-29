097bff7c735204fdeef4ef226286fca06cc9f1f2
Yash Sherekar <yashsherekar17@gmail.com>
2026-06-29 23:45:30 +0530

    Practical implementation of with_strutured_output() functiont using Pydantic

--- Langchain Structured Output/4.with_structured_output_pydantic.py
+++ Langchain Structured Output/4.with_structured_output_pydantic.py
@@ -32,7 +32,7 @@ Stunning 200MP camera with incredible zoom capabilities
 Long battery life with fast charging
 S-Pen support is unique and useful
                                  
-Review by Nitish Singh
+Review by Yash Sherekar
 """)
 
 print(result)
