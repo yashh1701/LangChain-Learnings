b86938ad4833f62b05092400afb48db4fafe2ae8
Yash Sherekar <yashsherekar17@gmail.com>
2026-07-06 23:13:45 +0530

    Practical implementation of Output Parsers using OpenAI with chaining

--- Output Parsers/2.StrOutParserChaining_OpenAI.py
+++ Output Parsers/2.StrOutParserChaining_OpenAI.py
@@ -16,14 +16,17 @@ template1 = PromptTemplate(
  
 # 2nd prompt -> summary
 template2 = PromptTemplate(
-    template='Write a 5 line summary on the following text. /n {text}',
+    template='Write a 1 line summary on the following text. /n {text}',
     input_variables=['text']
 )
-  
+
+#creating a parser to convert the output of the model into a string
 parser = StrOutputParser()
 
+#creating a chain of prompts, models and parsers to get the final output
 chain = template1 | model | parser | template2 | model | parser
 
+#invoking the chain with the input variable 'topic' and printing the final output
 result = chain.invoke({'topic':'black hole'})
- 
+
 print(result)
