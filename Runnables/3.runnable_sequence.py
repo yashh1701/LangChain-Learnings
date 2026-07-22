151a230584c6fedf38b04a0f9fb80e46ca096954
Yash Sherekar <yashsherekar17@gmail.com>
2026-07-22 23:05:19 +0530

    Practical coding of RunnableSequence

--- Runnables/3.runnable_sequence.py
+++ Runnables/3.runnable_sequence.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from langchain.schema.runnable import RunnableSequence
+from langchain_core.runnables import RunnableSequence
 
 load_dotenv()
 
