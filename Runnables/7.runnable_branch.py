c83883005e5e436e9713b143afb105d6923d85e8
Yash Sherekar <yashsherekar17@gmail.com>
2026-07-28 23:47:51 +0530

    Practical coding of RunnableBranch

--- Runnables/7.runnable_branch.py
+++ Runnables/7.runnable_branch.py
@@ -6,11 +6,13 @@ from langchain_core.runnables import RunnableSequence, RunnableParallel, Runnabl
 
 load_dotenv()
 
+# This example demonstrates the use of RunnableBranch to conditionally execute different chains based on the length of the generated report. If the report exceeds 300 words, it will be summarized; otherwise, it will be passed through as is.
 prompt1 = PromptTemplate(
     template='Write a detailed report on {topic}',
     input_variables=['topic']
 )
 
+# This prompt is used to summarize the report if it exceeds 300 words.
 prompt2 = PromptTemplate(
     template='Summarize the following text \n {text}',
     input_variables=['text']
@@ -20,13 +22,16 @@ model = ChatOpenAI()
 
 parser = StrOutputParser()
 
+# The report generation chain generates a detailed report based on the provided topic.
 report_gen_chain = prompt1 | model | parser
 
+# The branch_chain checks the length of the generated report. If it exceeds 300 words, it will summarize the report using prompt2; otherwise, it will pass the report through unchanged.
 branch_chain = RunnableBranch(
     (lambda x: len(x.split())>300, prompt2 | model | parser),
     RunnablePassthrough()
 )
 
+# The final chain combines the report generation and the conditional branching into a single sequence. It first generates the report and then decides whether to summarize it or not based on its length.
 final_chain = RunnableSequence(report_gen_chain, branch_chain)
 
 print(final_chain.invoke({'topic':'Russia vs Ukraine'}))
