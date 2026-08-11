2b9b28d2d9d38c86eed51e598fa22ea960b84f89
Yash Sherekar <yashsherekar17@gmail.com>
2026-08-11 23:47:58 +0530

    Practical coding of CSVLoader

--- Document Loaders/5.CSVLoader.py
+++ Document Loaders/5.CSVLoader.py
@@ -1,5 +1,6 @@
 from langchain_community.document_loaders import CSVLoader
 
+# a simple example of loading a CSV file and printing the number of documents and the second document
 loader = CSVLoader(file_path='Social_Network_Ads.csv')
 
 docs = loader.load()
